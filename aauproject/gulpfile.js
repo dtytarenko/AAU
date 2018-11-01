ef48e382ad7ddf7a8db4b5863b7eac9cd8389fa7
DmitriyTitarenko <dmutro.tutarenko@gmail.com>
2018-11-01 17:02:20 +0200

    01.11.

--- aauproject/gulpfile.js
+++ aauproject/gulpfile.js
@@ -1,9 +1,9 @@
 var gulp         = require('gulp'),  //основной плагин gulp
 	stylus       = require('gulp-stylus'),  //препроцессор stylus
-	browserSync  = require('browser-sync'),
-	concat       = require('gulp-concat'),
+	browserSync  = require('browser-sync'), 
+	concat       = require('gulp-concat'), // конкатенация (используется для js)
 	uglify       = require('gulp-uglifyjs'),  //минификация js
-	cssnano      = require('gulp-cssnano'),
+	cssnano      = require('gulp-cssnano'), 
 	rename       = require('gulp-rename'),
 	del          = require('del'),
 	autoprefixer = require('gulp-autoprefixer');  //расставление автопрефиксов
@@ -31,14 +31,6 @@ gulp.task('scripts', function() {
 	.pipe(gulp.dest('app/js'));
 });
 
-gulp.task('css-libs', ['stylus'], function() {
-	return gulp.src('app/css/libs.css')
-	.pipe(cssnano())
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('app/css'));
-});
-
-
 gulp.task('browser-sync', function() {
 	browserSync({
 		server: {
@@ -48,36 +40,43 @@ gulp.task('browser-sync', function() {
 	});
 });
 
+
+/* gulp-clean для очистки сборочной директории 
+*/
 gulp.task('clean', function() {
 	return del.sync('dist');
 });
 
+
 gulp.task('img', function() {
 	return gulp.src('app/img/**/*')
-	.pipe(imagemin({
-		interlaced: true,
-		progressive: true,
-		svgoPlugins: [{removeViewBox: false}],
-		// une: [pngquant()]
-	}))
+	.pipe(imagemin([
+		imagemin.gifsicle({interlaced: true}),
+		imagemin.jpegtran({progressive: true}),
+		imagemin.optipng({optimizationLevel: 5}),
+		imagemin.svgo({
+		  plugins: [
+			  {removeViewBox: true},
+			  {cleanupIDs: false}
+		  ]
+		})
+	]))
 	.pipe(gulp.dest('dist/img'));
 });
 
 // таск для билдинга html
 gulp.task('html:dist', function () {
-    gulp.src(path.app.include.html) //Выберем файлы по нужному пути
+    gulp.src(path.app.html) //Выберем файлы по нужному пути
         .pipe(rigger()) //Прогоним через rigger
         .pipe(gulp.dest(path.dist.html)) //выгрузим их в папку build
         .pipe(browserSync.reload());  //И перезагрузим наш сервер для обновлений
 });
 
-gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
+gulp.task('watch', ['browser-sync', 'scripts', 'stylus'], function() {
 	gulp.watch('app/stylus/**/*.styl', ['stylus']);
 	gulp.watch('app/*.html', browserSync.reload);
 	gulp.watch('app/js/**/*.html', browserSync.reload);
 })
-
-
 
 gulp.task('build', ['clean', 'stylus', 'scripts'], function() {
 	var buildCss = gulp.src([
@@ -94,5 +93,4 @@ gulp.task('build', ['clean', 'stylus', 'scripts'], function() {
 
 	var buildHtml = gulp.src('app/*.html')
 		.pipe(gulp.dest('dist'));
-
 })
