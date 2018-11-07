19e5293e244d8d493798c9009ba53a8c90b3a75e
DmitriyTitarenko <dmutro.tutarenko@gmail.com>
2018-11-07 18:27:02 +0200

    end 07.11, war with adaptive

--- dist/main.js
+++ dist/main.js
@@ -2,9 +2,12 @@ $( document ).ready(function() {
     $( ".menu-burger" ).click(function() {
         $( ".menu" ).slideToggle( "slow");
         $('.menu').css('display', 'flex');
+        $('.menu').css('z-index', '4');
+        $('.overlay').slideToggle("slow");
     });
     
-    $( ".menu__close" ).click(function() {
+    $( ".menu__close, .overlay" ).click(function() {
         $( ".menu" ).slideToggle( "slow");
+        $('.overlay').slideToggle("slow");
     });
 });
