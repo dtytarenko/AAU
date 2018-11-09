43f09416ef34eb4db90d07cd0043864a48a92c78
DmitriyTitarenko <dmutro.tutarenko@gmail.com>
2018-11-09 11:31:58 +0200

    end account

--- dist/main.js
+++ dist/main.js
@@ -3,11 +3,26 @@ $(document).ready(function() {
         $( ".menu" ).slideToggle( "slow");
         $('.menu').css('display', 'flex');
         $('.menu').css('z-index', '4');
-        $('.overlay').slideToggle("slow");
+        if(window.innerWidth >= 321) {
+            $('.overlay').slideToggle("slow");
+        } // it`s for delete overlay for mobile-width
     });
     
-    $( ".menu__close, .overlay" ).click(function() {
+    $( ".menu__close, .overlay, .menu__close-img--mob" ).click(function() {
         $( ".menu" ).slideToggle( "slow");
-        $('.overlay').slideToggle("slow");
+        if(window.innerWidth >= 321) {
+            $('.overlay').slideToggle("slow");
+        }
     });
 });
+
+
+$(window).bind('resize', function(e)
+{
+  if (window.RT) clearTimeout(window.RT);
+  window.RT = setTimeout(function()
+  {
+    this.location.reload(false); /* false to get page from cache */
+  }, 200);
+}); 
+// it`s for refresh the page on a browser resize using (important for menu in account-type pages)
