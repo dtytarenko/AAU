6acd7895486622d3e4aa3848af08228f90983739
DmitriyTitarenko <dmutro.tutarenko@gmail.com>
2018-12-27 19:08:33 +0200

    make markup for account personal-info

--- dist/js/account.js
+++ dist/js/account.js
@@ -7,5 +7,10 @@ $(document).ready(function() {
     
     $( ".menu__close, .overlay, .menu__close-img--mob" ).click(function() {
         $( ".menu, .overlay" ).slideToggle( "slow");
+        if(window.innerWidth >= 321) {
+            $('.overlay').style.display = "none";
+        }
     });
-}); 
+});
+
+$('.personal-info__city > option:first').hide();
