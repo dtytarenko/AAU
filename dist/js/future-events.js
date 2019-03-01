49c70e1d9f0876e4ae89ce7010e27f7575826d72
http404ntfnd <dmutro.tutarenko@gmail.com>
2019-03-01 17:39:59 +0200

    chanche arch, add callendar.page, done future-evenst

--- dist/js/future-events.js
+++ dist/js/future-events.js
@@ -1,10 +1,6 @@
 
    var Imtech = {};
    Imtech.Pager = function() {
-   
-   
-   
-   
        this.paragraphsPerPage = 3;
        this.currentPage = 1;
        this.pagingControlsContainer = '#pagingControls';
@@ -20,10 +16,7 @@
    
            return numPages;
        };
-       
-       
-       
-       
+
    // page - текущая (открытая - номер) страница, то есть в ф-ю передаем номер текущий страницы, контент котор впоследствии выводим
        this.showPage = function(page) {
            this.currentPage = page;
@@ -39,10 +32,7 @@
    //                          #pagingControls,  текущая страница(по умолч. 1), общее число страниц     
            renderControls(this.pagingControlsContainer, this.currentPage, this.numPages());
        }
-       
-       
-       
-       
+
    // блок с навигацией
        var renderControls = function(container, currentPage, numPages) {
    // разметка с навигацией
@@ -59,10 +49,59 @@
    
            $(container).html(pagingControls);
        }
-       
-       
-       
-       
-       
+ 
    }   
-          
+
+// calendar js
+var monthNames = ['Січень','Лютий','Березень','Квітень','Травень','Червень','Липень','Серпень','Вересень','Жовтень','Листопад','Грудень'];
+var calendarDate = new Date()
+var monthNum = calendarDate.getMonth();
+var prevMonth = monthNames[monthNum - 1];
+var nextMonth = monthNames[monthNum + 1];
+
+   $(document).ready(function() {
+    $('#calendar').fullCalendar({
+        firstDay: 1, /* первый день  недели */ 
+        defaultView: 'month',
+        header: {
+            left: 'prev,next',
+            center: 'title',
+        },
+        viewRender: function (view, element) {
+            var monthNames = ['Січень','Лютий','Березень','Квітень','Травень','Червень','Липень','Серпень','Вересень','Жовтень','Листопад','Грудень'];
+            var currentMonth = $("#calendar").fullCalendar('getDate').month();
+            var customMonth = monthNames[currentMonth];
+            $('.fc-prev-button, .fc-next-button').click(function(){
+                prevMonth = monthNames[customMonth - 1];
+                nextMonth = monthNames[currentMonth + 1];
+            });
+        },
+        monthNames: ['Січень','Лютий','Березень','Квітень','Травень','Червень','Липень','Серпень','Вересень','Жовтень','Листопад','Грудень'],
+        monthNamesShort: ['Січ.','Лют.','Бер.','Квіт.','Трав.','Черв.','Лип.','Серп.','Вер.','Жовт.','Лис.','Груд.'],
+        dayNames: ["Неділя","Понеділок","Вівторок","Середа","Четвер","П'ятниця","Субота"],
+        dayNamesShort: ["НД","ПН","ВТ","СР","ЧТ","ПТ","СБ"],
+        buttonText: {
+            prev: prevMonth,
+            next: nextMonth
+        },
+    });
+    
+    $('.fc-prev-button, .fc-next-button').click(function(){
+        var currentMonth = $("#calendar").fullCalendar('getDate').month();
+        var customMonth = monthNames[currentMonth];
+        prevMonth = monthNames[customMonth - 1];
+        nextMonth = monthNames[currentMonth + 1];
+    });
+});
+
+// tabs
+
+$(document).ready(function() {
+    $('.calendar-content__descr-list--item').click(function() {
+        $(this).find('.calendar-content__descr-list--item__descr').slideToggle( "slow");
+        $(this).find('.calendar-content__descr-list--item__descr').css('display', 'flex');
+        $(this).toggleClass('active');
+    });
+});
+
+// tabs
